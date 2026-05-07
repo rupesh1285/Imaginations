8bf8c55ba45ad4acec659255c115d28e2cfab80a
Rupesh Agarwal <rupeshagarwal728@gmail.com>
2026-05-07 19:27:54 +0530

    Updated some designs

--- src/components/home/Newsletter.tsx
+++ src/components/home/Newsletter.tsx
@@ -42,8 +42,32 @@ export default function Newsletter() {
           whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: true }}
           transition={{ duration: 0.8 }}
-          className="flex flex-col items-start text-left lg:pr-10 z-30"
+          className="flex flex-col items-start text-left lg:pr-10 z-30 relative"
         >
+           {/* Subtle Scrapbook Decorations */}
+           <div className="absolute -top-14 left-2 w-full h-16 pointer-events-none">
+              {/* Gold Sparkle 1 */}
+              <svg className="absolute top-2 left-0 w-4 h-4 rotate-12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 0C12 0 12 10.5 24 12C24 12 13.5 12 12 24C12 24 12 13.5 0 12C0 12 10.5 12 12 0Z" fill="#D4AF37" opacity="0.7"/>
+              </svg>
+              {/* Gold Sparkle 2 */}
+              <svg className="absolute top-8 left-12 w-3 h-3 rotate-45" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 0C12 0 12 10.5 24 12C24 12 13.5 12 12 24C12 24 12 13.5 0 12C0 12 10.5 12 12 0Z" fill="#D4AF37" opacity="0.5"/>
+              </svg>
+              {/* Gold Sparkle 3 */}
+              <svg className="absolute -top-2 left-24 w-5 h-5 -rotate-12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 0C12 0 12 10.5 24 12C24 12 13.5 12 12 24C12 24 12 13.5 0 12C0 12 10.5 12 12 0Z" fill="#D4AF37" opacity="0.6"/>
+              </svg>
+              {/* Rose Petal 1 */}
+              <svg className="absolute top-6 -left-4 w-6 h-6 rotate-[30deg]" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 2C16 2 21 7 21 13C21 19 16 22 12 22C6 22 3 16 3 11C3 6 8 2 12 2Z" fill="#F9C5D1" opacity="0.9"/>
+              </svg>
+              {/* Rose Petal 2 */}
+              <svg className="absolute top-2 left-16 w-5 h-5 -rotate-[15deg]" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 2C16 2 21 7 21 13C21 19 16 22 12 22C6 22 3 16 3 11C3 6 8 2 12 2Z" fill="#F9C5D1" opacity="0.7"/>
+              </svg>
+           </div>
+
            <span className="font-miniver text-4xl md:text-5xl text-[#B76E79] mb-[-10px] ml-2 drop-shadow-sm">Accept the</span>
            <h2 className="text-5xl md:text-7xl font-medium text-[#5C3A46] leading-none mb-6 font-serif tracking-tight drop-shadow-sm italic">
              INVITATION
